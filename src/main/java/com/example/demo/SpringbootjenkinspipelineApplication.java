37dfe5a3d847c22a6a73c78bc10b3f505053a19f
Skpandey108 <81761533+Skpandey108@users.noreply.github.com>
2023-07-09 17:04:47 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootjenkinspipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjenkinspipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjenkinspipelineApplication.class, args);
+	}
+
+}
